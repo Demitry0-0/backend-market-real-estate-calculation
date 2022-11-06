61f1240de52d4e1e9a80d8bc1e9c0468200b2ed1
Demitry0-0 <58517416+Demitry0-0@users.noreply.github.com>
2022-11-06 22:37:00 +0300

    Delete test.php

--- parse/test.php
+++
@@ -1,2 +0,0 @@
-https://move.ru/kvartiry_v_novostroykah/v_monolitnom_dome/?subcategory=1&rooms%5B0%5D=2&floor_min=6&floor_max=14&page=1&limit=50&sort=3&lat_user=55.721265&lng_user=37.454753
-https://move.ru/kvartiry_v_novostroykah/v_monolitnom_dome/?floor_min=6&floor_max=14%2F&subcategory=1&rooms%5B0%5D=2&page=1&limit=50&sort=3&lat_user=55.721265&lng_user=37.454753
